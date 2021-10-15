6082972c8727fb4afcfa86944b0728551b4f3864
naseebali_na2 <naseebalina2@gmail.com>
2021-10-15 14:50:56 +0500

    take pull form main

--- resources/views/shared/eventschedule/index.blade.php
+++ resources/views/shared/eventschedule/index.blade.php
@@ -1,51 +1,51 @@
-@foreach(Helpers::teamsInAgeGroups($eventResults) as $eventdata)           
+@foreach(Helpers::teamsInAgeGroups($eventResults) as $eventdata)
             <section class="custom-background" style="min-height: 100px;">
                     <div class="container-fluid">
-                
+
                     <div class="row parent-overlay">
-                        
+
                             <div class="col-lg-5 custom-column ">
-                                
+
                                 <div class="flex-container">
                                     <div class="az">
                                         <span class="badge badge-pill badge-warning px-4 py-2">{{$eventdata->state->state_name}}</span>
                                     </div>
                                     <div class="dates ">
-                                    {{ date('M-d-Y', strtotime($eventdata->created_at))  }}
+                                        {{ date('y-M-d', strtotime($eventdata->start_date)).'-'.date('d', strtotime($eventdata->end_date))  }}
                                     </div>
                                     <div class="florida">
                                     {{ $eventdata->event_name }}
-                                    </div>  
+                                    </div>
                                 </div>
                             </div>
                             <div class="col-lg-4 custom-column">
                                 <div class="flex-container">
-                            
-                                    <div class="age-group"> 
-                                
-                                        @foreach($eventdata->ageGroups as $agegroup) 
+
+                                    <div class="age-group">
+
+                                        @foreach($eventdata->ageGroups as $agegroup)
                                         <form action="{{ route('agegroupteams') }}" method="post">
-                                            @csrf 
-                                            <input type="hidden" name="agegroupId" value="{{ $agegroup['id'] }}">                               
-                                            <input type="hidden" name="eventId" value="{{ $eventdata->id }}"> 
+                                            @csrf
+                                            <input type="hidden" name="agegroupId" value="{{ $agegroup['id'] }}">
+                                            <input type="hidden" name="eventId" value="{{ $eventdata->id }}">
                                             <button type="submit" class="badge badge-secondary" style="width:50px;tex-align:center">{{ $agegroup['age_group'] }}</button>
-                                            <br>   
-                                        </form>                             
-                                        @endforeach 
-                                    
-                                    </div>                  
+                                            <br>
+                                        </form>
+                                        @endforeach
+
+                                    </div>
                                     <div class="age-group ">
-                                  
+
                                     @foreach($eventdata->checkageGroupStatus as $agegroupstatus)
-                                
+
                                     @if($agegroupstatus == "open")
                                     <span class="badge badge-pill badge-success " style="width:50px;tex-align:center">Open</span><br>
                                     @else
                                     <span class="badge badge-pill badge-info  " style="width:50px;tex-align:center">Close</span><br>
-                                    @endif                          
-                                    @endforeach 
-                                    </div> 
-                            
+                                    @endif
+                                    @endforeach
+                                    </div>
+
                                     <div class="AuBurnDale">
                                         @if($eventdata->approved == 1) <p style="color:green">Approved</p> @else  <p>Pending</p> @endif
                                     </div>
@@ -53,13 +53,13 @@
                             </div>
                             <div class="col-lg-3 custom-column">
                                 <div class="flex-container">
-                                
-                                    <div>  
+
+                                    <div>
                                         <i class="fas fa-map-marker-alt"></i>
                                     </div>
                                     <div class="lanwood-link">
                                         <a href="">{{ $eventdata->event_city }}</a>
-                                    </div>  
+                                    </div>
                                     <div class="overlay">
                             <a href="{{ route('event.edit',$eventdata->id)}}"> <i class="fas fa-edit"></i></a> <br>
                             <a href="{{ route('event.show', $eventdata->id) }}"> <i class="fas fa-eye  "></i></a> <br>
@@ -68,7 +68,7 @@
                                 </div>
                             </div>
                         </div>
-                
+
                     </div>
 
             </section>
