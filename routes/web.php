c41e1e75aca9cc413d8c464b684e4e7e841a2a02
naseebali_na2 <naseebalina2@gmail.com>
2021-10-25 18:03:30 +0500

    search player in frontend

--- routes/web.php
+++ routes/web.php
@@ -82,18 +82,15 @@ route::get('about-us', [HomeController::class, 'aboutUs'])->name('aboutus');
 Route::any('blog/{blog}', [HomeController::class, 'showBlog'])->name('blog');
 Route::view('/profile', 'frontend.pages.player.profile');
 route::get('view/event/{id}', [FrontendEventController::class, 'viewEvent'])->name('view.event');
-<<<<<<< HEAD
-route::get('liveevents', [FrontendEventController::class, 'liveEvents'])->name('liveevents');
+Route::get('liveevents', [FrontendEventController::class, 'liveEvents'])->name('liveevents');
 Route::get('players-in-home', [HomeController::class, 'playersInHome'])->name('players_in_home');
 Route::post('search-player-in-home', [HomeController::class, 'playersearchinHome'])->name('search_player_in_home');
 Route::get('view-player-profile-in-home/{id}', [HomeController::class, 'playerProfileinHome'])->name('view_player_profile_in_home');
-=======
-    Route::get('4baggerer-recent-content-detail/{id}', [HomeController::class, 'recentContentDetail'])->name('recentcontentdetail');
-    Route::get('userpayforsubscription/{id}', [HomeController::class, 'subscriptionForm'])->name('userpayforsubscription');
-    Route::post('subscribeplan', [HomeController::class, 'userPayForSubscribePlan'])->name('subscribeplan');
-    
+Route::get('4baggerer-recent-content-detail/{id}', [HomeController::class, 'recentContentDetail'])->name('recentcontentdetail');
+Route::get('userpayforsubscription/{id}', [HomeController::class, 'subscriptionForm'])->name('userpayforsubscription');
+Route::post('subscribeplan', [HomeController::class, 'userPayForSubscribePlan'])->name('subscribeplan');
 
->>>>>>> 008f2b4d1ed0e9782fda57741eefa091ec2aa298
+
 
 /**
  * Admin Routes
