9f9eb1f65d3a53c68175675b6668116746fa88b0
Add CRUD <hassi5466@gmail.com>
2021-12-01 15:04:10 +0500

    taking pull

--- resources/views/admin/pages/frontend/slider/create.blade.php
+++ resources/views/admin/pages/frontend/slider/create.blade.php
@@ -24,7 +24,7 @@
                     <div class="col-sm-12 mt-2 p-4">
                         <div class="row mb-2">
                             
-                                <div class="col-sm-6">
+                                <div class="col-sm-4">
                                     <label class="label__wrapper required">Title One</label>
                                     <input type="text" name="title_one" class="form-control input__box--wrapper" value="@if(isset($slider->title_one)) {{ $slider->title_one }} @endif" >
                                     @if($errors->has('title_one'))
@@ -33,7 +33,7 @@
                                         </span>
                                     @endif
                                 </div>
-                                <div class="col-sm-6">
+                                <div class="col-sm-4">
                                     <label class="label__wrapper required">Title Two</label>
                                     <input type="text" name="title_two" class="form-control input__box--wrapper" value="@if(isset($slider->title_two)) {{ $slider->title_two }} @endif" >
                                     @if($errors->has('title_two'))
@@ -42,33 +42,32 @@
                                         </span>
                                     @endif
                                 </div>
+                        <div class="col-sm-4">
+                        <label class="label__wrapper ">Cover Photo </label>
+                        <div class="custom-file">
+                                <input type="file" name= @if(isset($slider->cover_photo)) {{"update_photo" }} @else {{ "cover_photo" }} @endif  id="cover_photo" class="form-control input__box--wrapper custom-file-input" id="team_profile" accept="image/*">
+                                <span class="custom-file-label" for="validatedCustomFile">Choose team profile...</span>
+                                @if($errors->has('cover_photo'))
+                                <span class="invalid-feedback">
+                                    <strong>{{ $errors->first('cover_photo') }}</strong>
+                                </span>
+                                @endif
+                            </div>
+                      
+                    </div>
                             </div>
 
                             <div class="row mb-2">
-                                <div class="col-sm-9">
+                                <div class="col-sm-12">
                                     <label class="label__wrapper required">Content</label>
-                                    <textarea  name="content" class="form-control input__box--wrapper" >@if(isset($slider->content)) {{ $slider->content }} @endif</textarea>
+                                    <textarea  name="content" id="summernote" class="form-control input__box--wrapper" >@if(isset($slider->content)) {{ $slider->content }} @endif</textarea>
                                     @if($errors->has('content'))
                                         <span class="invalid-feedback">
                                             <strong>{{ $errors->first('content') }}</strong>
                                         </span>
                                     @endif
                                 </div>
-                                <div class="col-sm-3">
-                                    <div class="form-row float-right" style="margin-top:30px;">
-                                        <div class="form-group col-md-8 col__mr-right mb-0">
-                                            <span class="btn btn__wrapper--four btn-file" id="btnfile">
-                                                Cover Photo <input type="file" id="cover_photo"  name= @if(isset($slider->cover_photo)) {{"update_photo" }} @else {{ "cover_photo" }} @endif>
-                                            </span>
-                                            
-                                        </div>
-                                    </div>
-                                    @if($errors->has('cover_photo'))
-                                            <span class="invalid-feedback">
-                                                <strong>{{ $errors->first('cover_photo') }}</strong>
-                                            </span>
-                                            @endif
-                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -79,4 +78,12 @@
         </form>
     </div>
 </div>
+<script>
+    $('#summernote').summernote({
+      placeholder: 'Enter Official Partner Detail',
+      tabsize: 2,
+      height: 200
+    });
+
+ </script>
 @endsection
